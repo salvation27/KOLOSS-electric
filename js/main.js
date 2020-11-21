a9fb4cd7bd62a0f717c06caa8b83b30ec3a657cc
Nikolay Serdun <nikolayzalipykin@gmail.com>
2020-11-21 19:55:04 +0200

    19/54(21.11.2020)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 500,
+});
+
+
+$('.brend').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll:1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 // Material Select Initialization
 $(document).ready(function() {
 $('.mdb-select').materialSelect();
@@ -76,3 +128,5 @@ $("#myModal").on("show", function () {
 }).on("hidden", function () {
    $("body").removeClass("modal-open")
 });
+
+
