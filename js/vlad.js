f149855f9d0d95804f9e096f95f43c20e199ce45
Nikolay Serdun <nikolayzalipykin@gmail.com>
2020-11-22 21:42:39 +0200

    21/40(22.11.2020)

--- js/vlad.js
+++ js/vlad.js
@@ -18,7 +18,35 @@ $('.slider-for').slick({
     asNavFor: '.slider-for',
     dots: true,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [
+    {
+      breakpoint: 1082,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 
 
